59e70c106d2786cbfba23cab1fc6fe4921f3a389
TessalonicaPutryAvrylya <tessalonicaputry@gmail.com>
2023-05-16 06:28:53 +0700

    Add existing project files to Git

---
+++ app/src/main/java/com/example/project_ecommerce/AboutUsActivity.java
@@ -0,0 +1,39 @@
+package com.example.project_ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+    public void whatsapp(View view) {
+        int viewID = view.getId();
+        String number = null;
+        switch (viewID){
+            case R.id.wa_tessa:
+                number = "62895325393060";
+                break;
+            case R.id.wa_alpha:
+                number = "6287719857757";
+                break;
+            case R.id.wa_raul:
+                number = "6281338442260";
+                break;
+            default:
+                break;
+        }
+        String url = "https://api.whatsapp.com/send?phone="+number;
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+}
